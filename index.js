ba0ed472af490cfd14a9681543a521fdb808d125
David Halsey <dhalsey212@gmail.com>
2018-01-07 08:31:13 -0500

    Require cherio and cli-table for jquery syntax and cli result printout

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 //make ajax requests from other websites
-const rp = require('request-promise')
+const rp = require('request-promise');
+//use jQuery similar syntax in node
+const cherio = require('cherio');
+//displays results in console
+const Table = require('cli-table');
