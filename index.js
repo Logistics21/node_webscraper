73080118e072041f4e54b65df708a43b0d95a216
David Halsey <dhalsey212@gmail.com>
2018-01-07 08:55:01 -0500

    Parse html for email addresses and fix typos

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 //make ajax requests from other websites
 const rp = require('request-promise');
 //use jQuery similar syntax in node
-const cherio = require('cherio');
+const cheerio = require('cheerio');
 //displays results in console
 const Table = require('cli-table');
 
+const ignore = [
+  ".*@example.com$",
+  ".*@domain.com$",
+  ".*@mydomain.com$",
+  ".*\.png$",
+  ".*\.jpe?g$",
+  ".*\.gif$",
+  ".*@[0-9\.]+$" // no letters
+];
+
+const emails = [];
+
 const options = {
-  uri: `https://www.google.com`,
-  json:true,
+  uri: `http://www.une.edu/registrar/staff-locations`, //test location
+  // uri: `https://www.google.com`,
   transform: function(body) {
     return cheerio.load(body);
   }
@@ -15,8 +27,21 @@ const options = {
 
 rp(options)
   .then(($) => {
-    console.log($);
+    process.stdout.write('loading');
+    const $a = $('a[href*=mailto]');
+
+    $a.each((i, el) => {
+	    var email = $(el).attr('href').slice(7);
+	    emails.push(email);
+    });
+
+    console.log(emails);
   })
+
   .catch((err) => {
     console.log(err);
   });
+
+const printEmails = () => {
+
+}
